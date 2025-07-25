780568572c5a0a49351a1fd42d875326aebf8cc5
Brandon L <brandon.lai.cheong@gmail.com>
2025-07-25 15:19:35 -0400

    fixed bug that hid app

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -4,21 +4,31 @@ import { PersonalInfo } from '@/utils/personalInfo';
 import { PersonalInfoFormStorage } from '@/utils/personalInfoFormStorage';
 
 function saveData() {
-  const form = document.querySelector('#personalinfo') as HTMLFormElement
-  const formData = new FormData(form);
 
-  formData.values()
+
+  let firstName = document.querySelector("#first-name")?.nodeValue ?? "empty"
+  let lastName = document.querySelector("#last-name")?.nodeValue ?? ""
+  let emailAddress = document.querySelector("#email-address")?.nodeValue ?? ""
+  let phoneNumber = document.querySelector("#phone-number")?.nodeValue ?? ""
+
+  let personalInfo = PersonalInfo.create({
+      firstName: firstName,
+      lastName: lastName,
+      emailAddress: emailAddress,
+      phoneNumber: phoneNumber
+    })
+  PersonalInfoFormStorage.setValues(personalInfo)
 }
 
 function App() {
 
-  const [loading, setLoading] = useState(false);
-  const [form, setForm] = useState(new PersonalInfo);
+  const [loading, setLoading] = useState(true);
+  const [form, setForm] = useState(PersonalInfo.create());
 
   useEffect(() => {
     PersonalInfoFormStorage.getValues().then(result => {
       setForm(result)
-      
+      setLoading(false)
     })
     return () => setLoading(true)
   }, [])
@@ -28,18 +38,20 @@ function App() {
       <h1>Auto Applier</h1>
       <h2>Your Resumes</h2>
       <h2>Personal Information</h2>
-      ({loading} ? (
-        <Skeleton/>
-        <Skeleton/>
-        <Skeleton/>
-        <Skeleton/>
+      {(loading ? (
+        <>
+          <Skeleton/>
+          <Skeleton/>
+          <Skeleton/>
+          <Skeleton/>
+        </>
       ) : <form id='personalinfo'>
           <TextField id='first-name' label='First Name' value={form.firstName}/>
           <TextField id='last-name' label='Last Name' value={form.lastName}/>
           <TextField id='email-address' label='Email Address' value={form.emailAddress}/>
           <TextField id='phone-number' label='Phone Number' value={form.phoneNumber}/>
-          <Button type='submit' onClick={saveData}>Save</Button>
-        </form>)
+          <Button onClick={saveData}>Save</Button>
+        </form>)}
     </>
   );
 }
