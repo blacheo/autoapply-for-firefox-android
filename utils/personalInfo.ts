a15f591fc89e7288353e569e7a4fd07062a6008c
Brandon L <brandon.lai.cheong@gmail.com>
2025-07-27 09:26:15 -0400

    added full name method to personal info

--- utils/personalInfo.ts
+++ utils/personalInfo.ts
@@ -5,4 +5,7 @@ export class PersonalInfo extends Data {
     lastName: string = "";
     emailAddress: string = "";
     phoneNumber: string = "";
+    fullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
 }
