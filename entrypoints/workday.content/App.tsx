76eebfaabc96371aef66e805c319cb92d92d3c59
Brandon L <brandon.lai.cheong@gmail.com>
2025-07-27 16:37:50 -0400

    workday autofill in progress

--- entrypoints/workday.content/App.tsx
+++ entrypoints/workday.content/App.tsx
@@ -1,10 +1,15 @@
 import { Button, Fab } from "@mui/material";
 import CreateIcon from '@mui/icons-material/Create';
+import { firstNameInput } from "./workdayLocations";
+
+function autofill() {
+    firstNameInput.
+}
 
 export function App() {
     return (
     <>
-        <Fab variant='extended'><CreateIcon sx={{ mr: 1 }}/>Autocomplete</Fab>
+        <Fab variant='extended' onClick={autofill}><CreateIcon sx={{ mr: 1 }}/>Autocomplete</Fab>
     </>
     )
 }
---
+++ entrypoints/workday.content/workdayLocations.ts
@@ -0,0 +1,4 @@
+// AKA given name
+export const firstNameInput = document.getElementById("name--legalName--firstName")
+
+export const lastNameInput = document.getElementById("name--legalName--lastName")
