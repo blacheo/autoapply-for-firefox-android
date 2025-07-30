edc851faa28eaf676834e36ca2065d8d5f5a81ab
Brandon L <brandon.lai.cheong@gmail.com>
2025-07-29 23:04:55 -0400

    refactored parts of popup into own component

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Fab, Step, StepButton, StepContent, Stepper } from '@mui/material';
+import { Fab, Step, StepButton, StepContent, Stepper, Typography } from '@mui/material';
 import { PersonalInfo } from '@/utils/personalInfo';
 import { PersonalInfoFormStorage } from '@/utils/personalInfoFormStorage';
 import '@fontsource/roboto/300.css';
@@ -8,22 +8,10 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { PersonalInfoComponent } from './components/PersonalInfoForm';
 import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
+import { ResumeNaming } from './components/ResumeNaming';
 
 function App() {
-
-  const [loading, setLoading] = useState(true);
-  const [form, setForm] = useState(PersonalInfo.create());
   const [activeStep, setActiveStep] = useState(0)
-
-  useEffect(() => {
-    PersonalInfoFormStorage.getValues().then(result => {
-      setForm(result)
-      console.log(result)
-      setLoading(false)
-    })
-    return () => setLoading(true)
-  }, [])
-
   const handleStep = (step: number) => () => {
     console.log(`handle step ${step}`)
     setActiveStep(step);
@@ -31,17 +19,14 @@ function App() {
 
   return (
     <>
-      <h1 className='text-base'>Auto Applier</h1>
-      <h2 className='text-base'></h2>
+      <Typography variant='h6'>Auto Applier</Typography>
       <Stepper nonLinear activeStep={activeStep} orientation='vertical'>
         <Step key="Select a Resume">
           <StepButton onClick={handleStep(0)}>
             Select a Resume
           </StepButton>
           <StepContent>
-            <input type="file" id="input" multiple />
-
-            <Fab><DriveFolderUploadIcon/></Fab>
+            <ResumeNaming />
           </StepContent>
         </Step>
 
@@ -50,7 +35,7 @@ function App() {
             Enter personal information
           </StepButton>
           <StepContent>
-            <PersonalInfoComponent loading={loading} form={form} />
+            <PersonalInfoComponent />
           </StepContent>
         </Step>
       </Stepper>
