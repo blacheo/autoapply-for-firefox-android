bdaebc810b76850e9b22525f369f100721457910
Brandon L <brandon.lai.cheong@gmail.com>
2025-07-30 15:26:11 -0400

    attempted to implement resume name

--- entrypoints/popup/components/ResumeSelection.tsx
+++ entrypoints/popup/components/ResumeSelection.tsx
@@ -1,3 +1,11 @@
-export function ResumeSelection() {
+import { NativeSelect } from "@mui/material";
 
+export function ResumeSelection() {
+    const [resumes, setResumes] = useState([]);
+    return (
+        <>
+            <NativeSelect>
+            </NativeSelect>
+        </>
+    )
 }
--- utils/resumeStorage.ts
+++ utils/resumeStorage.ts
@@ -1,10 +1,30 @@
 class ResumeStorage {
+    private static resumeName = storage.defineItem<string>('local:resume_name', { fallback: "" })
+
+
+    static async getResumeName() {
+        const name = await ResumeStorage.resumeName.getValue()
+        if (name === "") {
+            const personalInfo = await PersonalInfoFormStorage.getValues()
+
+            if (personalInfo.firstName !== "" && personalInfo.lastName !== "") {
+                const defaultName = `${personalInfo.firstName}_${personalInfo.lastName}`;
+                await ResumeStorage.resumeName.setValue(defaultName);
+                return defaultName;
+            }
+        }
+
+        return name;
+    }
+
+    static async setResumeName(name : string) {
+        await ResumeStorage.resumeName.setValue(name);
+    }
+
     // submitted resumes will use this file name
     getResumeDefaultName() {
 
     }
 
-    static storeFile(origFileName, file) {
-        storeFile(origFileName, file);
-    }
+    
 }
