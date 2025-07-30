30afccd929685b2134372c2a9b219d51549e75c5
Brandon L <brandon.lai.cheong@gmail.com>
2025-07-29 20:53:47 -0400

    boilerplate

---
+++ utils/resumeStorage.ts
@@ -0,0 +1,10 @@
+class ResumeStorage {
+    // submitted resumes will use this file name
+    getResumeDefaultName() {
+
+    }
+
+    static storeFile(origFileName, file) {
+        storeFile(origFileName, file);
+    }
+}
