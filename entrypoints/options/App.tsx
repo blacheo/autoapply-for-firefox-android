3e81794f615d3b9ed86e85e269234fe301759dc4
Brandon L <brandon.lai.cheong@gmail.com>
2025-08-05 09:31:39 -0400

    refactor + added next and back buttons to options

--- entrypoints/options/App.tsx
+++ entrypoints/options/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Stack, Step, StepButton, StepContent, Stepper, Typography } from '@mui/material';
+import { Button, Stack, Step, StepButton, StepContent, Stepper, Typography } from '@mui/material';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,9 +10,12 @@ import { ResumeSection } from './components/ResumeSection';
 import { ResumeSelection } from './components/ResumeSelection';
 
 function App() {
+  const steps = [
+    {task: "Select a Resume", element: <ResumeNaming />}, 
+    {task: "Enter your personal clientInformation", element: <PersonalInfoComponent />}];
+
   const [activeStep, setActiveStep] = useState(0)
   const handleStep = (step: number) => () => {
-    console.log(`handle step ${step}`)
     setActiveStep(step);
   };
 
@@ -24,42 +27,19 @@ function App() {
     <>
       <Typography variant='h5'>Auto Applier</Typography>
       <Stepper nonLinear activeStep={activeStep} orientation='vertical'>
-        <Step key="Select a Resume">
-          <StepButton onClick={handleStep(0)}>
-            Select a Resume
-          </StepButton>
-          <StepContent>
-            <Stack>
-              <ResumeSelection />
-              <ResumeSection />
-              <ResumeNaming />
-            </Stack>
+        {steps.map((value, index) => (
+          <Step>
+            <StepButton onClick={handleStep(index)}>
+              {value.task}
+            </StepButton>
+            <StepContent>
+              {value.element}
+              <Button disabled={activeStep === 0} onClick={handleStep(activeStep - 1)}>Back</Button>
+              <Button onClick={() => handleNext()}>Next</Button>
+            </StepContent>
 
-          </StepContent>
-        </Step>
-
-        <Step key="Fill personal information">
-          <StepButton onClick={handleStep(1)}>
-            Enter personal information
-          </StepButton>
-          <StepContent>
-            <PersonalInfoComponent />
-          </StepContent>
-        </Step>
-
-        <Step key={"Fill contact information"}>
-          <StepButton onClick={handleStep(2)}>
-            Fill contact information
-          </StepButton>
-
-        </Step>
-
-        <Step key={"Miscelaneous"}>
-          <StepButton onClick={handleStep(3)}>
-            Miscelaneous
-          </StepButton>
-
-        </Step>
+          </Step>
+        ))}
       </Stepper>
 
 
